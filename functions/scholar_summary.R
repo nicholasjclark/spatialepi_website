69a2d60a15acb6afea7d771b8c952f2c7b1fe9df
nicholasjclark <nicholas.j.clark1214@gmail.com>
2019-02-13 16:37:47 +1000

    update site with new scholar functionality

--- functions/scholar_summary.R
+++ functions/scholar_summary.R
@@ -2,31 +2,35 @@
 #'@param user Character string representing Google Scholar user profile ID
 #'@return A \code{ggplot2} object showing a graph of citation metrics, with a timestamp
 #' 
-scholar_summary = function(user){
+scholar_summary = function(author_list){
 library(scholar)
-cit <- scholar::get_citation_history(user)
-tot <- scholar::get_num_articles(user) 
-prof <- scholar::get_profile(user)
+library(dplyr)
+  
+citations <- do.call(rbind, lapply(seq_len(length(author_list)), function(x){
+  cit <- scholar::get_citation_history(author_list[x])
+})) %>%
+  dplyr::group_by(year) %>%
+  dplyr::summarise(cites = sum(cites))
 
-ggplot2:: ggplot(cit, ggplot2::aes(x = year, y = cites)) +
+tot_articles <- sum(unlist(lapply(seq_len(length(author_list)), function(x){
+  tot <- scholar::get_num_articles(author_list[x])
+})))
+
+ggplot2:: ggplot(citations, ggplot2::aes(x = year, y = cites)) +
   ggplot2::geom_bar(stat = 'identity') +
   ggplot2::theme_bw() +
   ggplot2::xlab('Year') +
   ggplot2::ylab('Google Scholar\n citations')+
+  ggplot2::annotate('text', label = 'SpatialEpiLab Publications',
+                    x = -Inf, y = Inf, hjust = -0.03, vjust = 1.5, size = 4) +
   ggplot2::annotate('text', label = format(Sys.time(), "%Y-%m-%d"),
-           x = -Inf, y = Inf, vjust = 1.5, hjust = -0.08, size = 3) +
-  ggplot2::annotate("text", x = -Inf, y = Inf, vjust = 3, hjust = -0.05, 
-                    label = paste('Total papers = ', tot),
+           x = -Inf, y = Inf, vjust = 4.5, hjust = -0.1, size = 3) +
+  ggplot2::annotate("text", x = -Inf, y = Inf, vjust = 6, hjust = -0.05, 
+                    label = paste('Total papers = ', tot_articles),
                     size = 3) +
-  ggplot2::annotate("text", x = -Inf, y = Inf, vjust = 4.5, hjust = -0.05, 
-           label = paste('Total ciatations = ', sum(cit$cites)),
+  ggplot2::annotate("text", x = -Inf, y = Inf, vjust = 7.5, hjust = -0.05, 
+           label = paste('Total citations = ', sum(citations$cites)),
            size = 3) +
-  ggplot2::annotate("text", x = -Inf, y = Inf, vjust = 6, hjust = -0.12, 
-                    label = paste('h index = ', prof$h_index),
-                    size = 3) + 
-  ggplot2::annotate("text", x = -Inf, y = Inf, vjust = 7.5, hjust = -0.12, 
-                    label = paste('i10 index = ', prof$i10_index),
-                    size = 3) + 
   ggplot2::theme(panel.grid.major = ggplot2::element_blank(), 
                  panel.grid.minor = ggplot2::element_blank(),
                  axis.text.x = ggplot2::element_text(angle = 45, hjust = 1))
