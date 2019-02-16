5290624df5d8187d164f156215074ae07df00e30
nicholasjclark <nicholas.j.clark1214@gmail.com>
2019-02-16 16:50:57 +1000

    add pandji

--- scholar_ids/scholar_ids.R
+++ scholar_ids/scholar_ids.R
@@ -2,6 +2,7 @@
 ricardo_id <- 'aAh0t1AAAAAJ&hl'
 nick_id <- '5bO9uxEAAAAJ'
 tarni_id <- 'yZ2YwK8AAAAJ&hl'
+pandji_id <- 'awhMGaAAAAAJ&hl'
 
 # All IDs
-scholar_ids <- c(ricardo_id, nick_id, tarni_id)
+scholar_ids <- c(ricardo_id, nick_id, tarni_id, pandji_id)
