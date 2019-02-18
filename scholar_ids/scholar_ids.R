1f4f52afc3f7146ad777740392c6de34577946ee
nicholasjclark <nicholas.j.clark1214@gmail.com>
2019-02-19 07:30:20 +1000

    add yalem

--- scholar_ids/scholar_ids.R
+++ scholar_ids/scholar_ids.R
@@ -3,6 +3,7 @@ ricardo_id <- 'aAh0t1AAAAAJ&hl'
 nick_id <- '5bO9uxEAAAAJ'
 tarni_id <- 'yZ2YwK8AAAAJ&hl'
 pandji_id <- 'awhMGaAAAAAJ&hl'
+yalem_id <- 'yVWRy2wAAAAJ&hl'
 
 # All IDs
-scholar_ids <- c(ricardo_id, nick_id, tarni_id, pandji_id)
+scholar_ids <- c(ricardo_id, nick_id, tarni_id, pandji_id, yalem_id)
