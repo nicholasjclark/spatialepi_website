d5d03b3a394b4c91be39d954c49b2d4d6b7164db
nicholasjclark <nicholas.j.clark1214@gmail.com>
2019-04-17 11:30:58 +1000

    add shovon scholar profile

--- functions/scholar_summary.R
+++ functions/scholar_summary.R
@@ -6,8 +6,82 @@ scholar_summary = function(author_list){
 library(scholar)
 library(dplyr)
   
+  #### Auxillary functions ####
+  get_resp <- function(url, attempts_left = 5) {
+    
+    stopifnot(attempts_left > 0)
+    
+    resp <- httr::GET(url, handle = getOption("scholar_handle"))
+    
+    # On a successful GET, return the response
+    if (httr::status_code(resp) == 200) {
+      resp
+    } else if (attempts_left == 1) { # When attempts run out, stop with an error
+      stop("Cannot connect to Google Scholar. Is the ID you provided correct?")
+    } else { # Otherwise, sleep a second and try again
+      Sys.sleep(1)
+      get_resp(url, attempts_left - 1)
+    }
+  }
+  
+  tidy_id <- function(id) {
+    if (length(id)!=1) {
+      id <- id[1]
+      msg <- sprintf("Only one ID at a time; retrieving %s", id)
+      warning(msg)
+    }
+    
+    ## Check with Google to set cookies
+    if (getOption("scholar_call_home")) {
+      sample_url <- "https://scholar.google.com/citations?user=B7vSqZsAAAAJ"
+      sink <- GET(sample_url)      
+      options("scholar_call_home"=FALSE, "scholar_handle"=sink)
+    }
+    
+    return(id)
+  }
+  
+  compose_url <- function(id, url_template) {
+    if (is.na(id)) return(NA_character_)
+    id <- tidy_id(id)
+    url <- sprintf(url_template, id)
+    
+    url
+  }
+  
+  get_cite_history <- function(id) {
+    library(rvest)
+    url_template <- "http://scholar.google.com/citations?hl=en&user=%s&pagesize=100&view_op=list_works"
+    url <- compose_url(id, url_template)
+    
+    ## A better way would actually be to read out the plot of citations
+    page <- get_resp(url) %>% read_html()
+    years <- page %>% html_nodes(xpath="//*/span[@class='gsc_g_t']") %>%
+      html_text() %>% as.numeric()
+    vals <- page %>% html_nodes(xpath="//*/span[@class='gsc_g_al']") %>%
+      html_text() %>% as.numeric()
+    if(length(years)>length(vals)){
+      # Some years don't have citations.
+      # We need to match the citation counts and years
+      # <a href="javascript:void(0)" class="gsc_g_a" style="left:8px;height:5px;z-index:9">\n  <span class="gsc_g_al">2</span>\n</a>
+      style_tags=page %>% html_nodes(css = '.gsc_g_a') %>%
+        html_attr('style')
+      # these z indices seem to be the indices starting with the last year
+      zindices=as.integer(stringr::str_match(style_tags, 'z-index:([0-9]+)')[,2])
+      # empty vector of 0s
+      allvals=integer(length=length(years))
+      # fill in
+      allvals[zindices]=vals
+      # and then reverse
+      vals=rev(allvals)
+    }
+    df <- data.frame(year=years, cites=vals)
+    
+    return(df)
+  }
+  
 citations <- do.call(rbind, lapply(seq_len(length(author_list)), function(x){
-  cit <- scholar::get_citation_history(author_list[x])
+  cit <- get_cite_history(author_list[x])
 })) %>%
   dplyr::group_by(year) %>%
   dplyr::summarise(cites = sum(cites))
--- scholar_ids/scholar_ids.R
+++ scholar_ids/scholar_ids.R
@@ -5,6 +5,8 @@ tarni_id <- 'yZ2YwK8AAAAJ&hl'
 pandji_id <- 'awhMGaAAAAAJ&hl'
 yalem_id <- 'yVWRy2wAAAAJ&hl'
 tuhin_id <- '8Jpp1CAAAAAJ&hl'
+shovon_id <- 'SJx37JsAAAAJ&hl'
 
 # All IDs
-scholar_ids <- c(ricardo_id, nick_id, tarni_id, pandji_id, yalem_id, tuhin_id)
+scholar_ids <- c(ricardo_id, nick_id, tarni_id, pandji_id, yalem_id, tuhin_id,
+                 shovon_id)
