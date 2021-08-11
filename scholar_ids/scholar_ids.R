f67e0766be5c17d6f5ed3a1b87256eb7bbae0429
Tatiana Proboste Ibertti <uqtprobo@uq.edu.au>
2021-08-11 10:59:38 +1000

    Silvia id has been added

--- scholar_ids/scholar_ids.R
+++ scholar_ids/scholar_ids.R
@@ -10,7 +10,8 @@ philip_id <- 'BfWGpaUAAAAJ&hl'
 javier_id <- 'a0m2kUYAAAAJ&hl'
 tatiana_id <- '8k6aPjIAAAAJ&hl'
 uttara_id <- 'SfoR9d0AAAAJ&hl'
+silvia_id <- '5vV6OfUAAAAJ&hl'
 
 # All IDs
 scholar_ids <- c(ricardo_id, nick_id, tarni_id, shovon_id, philip_id, 
-                 javier_id, tatiana_id, uttara_id)
+                 javier_id, tatiana_id, uttara_id, silvia_id)
