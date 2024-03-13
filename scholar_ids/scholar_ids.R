7c0e2c850a00fc449c92bf84500c243ddcdc2fe5
Tatianaproboste <38739705+Tatianaproboste@users.noreply.github.com>
2024-03-13 12:20:06 +1000

    more updates and added Josefina's scholar ID

--- scholar_ids/scholar_ids.R
+++ scholar_ids/scholar_ids.R
@@ -1,18 +1,20 @@
 # Individual scholar IDs
 ricardo_id <- 'aAh0t1AAAAAJ&hl'
 nick_id <- '5bO9uxEAAAAJ'
+shovon_id <- 'SJx37JsAAAAJ&hl'
+philip_id <- 'BfWGpaUAAAAJ&hl'
+tatiana_id <- '8k6aPjIAAAAJ&hl'
+uttara_id <- 'SfoR9d0AAAAJ&hl'
+silvia_id <- '5vV6OfUAAAAJ&hl'
+josefina_id <- '0kBkQlcAAAAJ&hl'
+
 tarni_id <- 'yZ2YwK8AAAAJ&hl'
 pandji_id <- 'awhMGaAAAAAJ&hl'
 yalem_id <- 'yVWRy2wAAAAJ&hl'
 tuhin_id <- '8Jpp1CAAAAAJ&hl'
-shovon_id <- 'SJx37JsAAAAJ&hl'
-philip_id <- 'BfWGpaUAAAAJ&hl'
 javier_id <- 'a0m2kUYAAAAJ&hl'
-tatiana_id <- '8k6aPjIAAAAJ&hl'
-uttara_id <- 'SfoR9d0AAAAJ&hl'
-silvia_id <- '5vV6OfUAAAAJ&hl'
 
 
 # All IDs
 scholar_ids <- c(ricardo_id, nick_id, tarni_id, shovon_id, 
-                 tatiana_id, uttara_id, silvia_id)
+                 tatiana_id, uttara_id, silvia_id, josefina_id)
